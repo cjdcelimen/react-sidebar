80ec7e503ddcd10c39f0bdd6e07da863f8dda835
CJ Celimen <cjdcelimen@gmail.com>
2020-12-02 21:45:41 +0800

    Update Submenu and Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Sidebar from './components/Sidebar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Overview from './pages/Overview'
+import Reports from './pages/Reports'
+import Users from './pages/Users'
 
 function App() {
   return (
     <Router>
       <Sidebar />
+      <Switch>
+        <Route path='/overview' exact component={Overview} />
+        <Route path='/reports' exact component={Reports} />
+        <Route path='/overview/users' exact component={Users} />
+      </Switch>
     </Router>
   );
 }
--- src/components/SubMenu.js
+++ src/components/SubMenu.js
@@ -1,5 +1,4 @@
-import React from 'react'
-import { FaOtter } from 'react-icons/fa'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
@@ -25,17 +24,50 @@ const SidebarLabel = styled.span`
     margin-left: 16px;
 `;
 
+const DropdownLink = styled(Link)`
+    background: #414757;
+    height: 60px;
+    padding-left: 3rem;
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    color: #f5f5f5;
+    font-size: 18px;
+
+    &:hover {
+        background: #33302b;
+        cursor: pointer;
+    }
+`
+
 const SubMenu = ({ item }) => {
+    const [subnav, setSubnav] = useState(false)
+    const showSubnav = () => setSubnav(!subnav)
     return (
         <>
-            <SidebarLink to={item.path}>
+            <SidebarLink to={item.path} onClick={item.subNav && showSubnav}>
                 <div>
                     {item.icon}
                     <SidebarLabel>
                         {item.title}
                     </SidebarLabel>
                 </div>
+                <div>
+                    {item.subNav && subnav
+                        ? item.iconOpened
+                        : item.subNav
+                        ? item.iconClosed
+                        : null}
+                </div>
             </SidebarLink>
+            {subnav && item.subNav.map((item, index) => {
+                return (
+                    <DropdownLink to={item.path} key={index}>
+                        {item.icon}
+                        <SidebarLabel>{item.title}</SidebarLabel>
+                    </DropdownLink>
+                )
+            })}
         </>
     )
 }
